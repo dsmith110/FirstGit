4c29f8ab8844919f921fc0a7b57399f79a661699
dsmith110@my.wctc.edu <>
2013-01-24 17:01:13 -0600

    added a Test class and added a print statement

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -7,5 +7,6 @@ public class FirstGit {
     
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Good bye");
     }
 }
---
+++ src/firstgit/Test.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgit;
+
+/**
+ *
+ * @author dsmith110
+ */
+public class Test {
+    
+}
